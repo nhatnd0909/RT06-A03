758adde81aaf0ff1dd1ea8f7bb08c649485ae4ea
Nhat <nguyendinhnhat0909@gmail.com>
2023-05-25 11:15:28 +0700

    create view profile page

---
+++ src/java/control/ProfileControl.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package control;
+
+import dao.DAO;
+import entity.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProfileControl extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        DAO dao = new DAO();
+        Account account = dao.getAccountByID(id);
+        request.setAttribute("account", account);
+        request.getRequestDispatcher("profile.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    }
+}
